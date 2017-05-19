898acb0ee7b6aad9f1c9d47517cdcb7dbcf67f6f
maxikahe <max.ikaheimo@gmail.com>
2017-05-19 21:01:58 +0300

    Slightly Slower Than Light
    Kuopio games for health jam

---
+++ desktop/src/com/ikisoft/sstl/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ikisoft.sstl.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ikisoft.sstl.SSTL;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SSTL(), config);
+	}
+}
---
+++ html/src/com/ikisoft/sstl/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ikisoft.sstl.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ikisoft.sstl.SSTL;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SSTL();
+        }
+}
